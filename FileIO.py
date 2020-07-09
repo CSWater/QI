09ab2b096e7a6802bf9fe3627abdc598e3b695be
shuichaoyang <shuichaoyang@ncic.ac.cn>
2020-07-10 01:31:16 +0800

    add reverseFile to reverse the history data in increasing order (of date)

--- FileIO.py
+++ FileIO.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 def readFile(filename):
-    nums = np.array(pd.read_csv(filename, sep='\t'))
+    nums = np.array(pd.read_csv(filename))
     # 读取从英为财情读到的数据
     # date, close_price, open_price, high_price, low_price
     # trade_volume, rise_rate
@@ -11,3 +11,10 @@ def readFile(filename):
 def formatFile(filename):
   #TODO
   return
+
+def reverseFile(filename):
+  df = pd.read_csv(filename)
+  # date = df.date
+  # print(date)
+  df_reverse = df.sort_index(ascending=False)
+  df_reverse.to_csv(filename, index=False)
